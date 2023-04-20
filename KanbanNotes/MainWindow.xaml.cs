89484795c9c7b9bd14f2a12e107be87923f1f1cc
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-04-20 06:44:35 +0200

    Add a basic prototype of functionality

--- KanbanNotes/MainWindow.xaml.cs
+++ KanbanNotes/MainWindow.xaml.cs
@@ -12,17 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace KanbanNotes
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// <summary> Interaction logic for MainWindow.xaml </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void TaskBorder_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var taskBorder = (FrameworkElement)sender;
+
+            DragDrop.DoDragDrop(taskBorder, taskBorder, DragDropEffects.All);
+        }
+
+        private void TaskStack_Drop(object sender, DragEventArgs e)
+        {
+            var draggedTask = (FrameworkElement)e.Data.GetData(typeof(Border));
+            var fromPanel = (Panel)draggedTask.Parent;
+            var intoPanel = (Panel)sender;
+
+            fromPanel.Children.Remove(draggedTask);
+            intoPanel.Children.Add(draggedTask);
+        }
+
+        
     }
 }
