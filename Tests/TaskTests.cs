7753b51c293502eae3de2ff72e83a8179ee1da87
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-04-22 03:45:43 +0200

    Add tests for models

---
+++ Tests/TaskTests.cs
@@ -0,0 +1,14 @@
+namespace Tests;
+
+public class TaskTests
+{
+    public TaskTests()
+    {
+    }
+
+    [Fact]
+    public void TestClass()
+    {
+        throw new NotImplementedException();
+    }
+}
