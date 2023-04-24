6156a29a9a3c6770a203848f76db6792db5abd9e
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-04-24 08:18:58 +0200

    Refactor Column/Task to `App` styles

--- KanbanNotes/MainWindow.xaml.cs
+++ KanbanNotes/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace KanbanNotes;
+﻿using KanbanNotes.ViewModels;
+
+namespace KanbanNotes;
 
 /// <summary> Interaction logic for MainWindow.xaml </summary>
 public partial class MainWindow : Window
@@ -6,24 +8,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        DataContext = new MainWindowViewModel();
     }
-
-    private void TaskBorder_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-        var taskBorder = (FrameworkElement)sender;
-
-        DragDrop.DoDragDrop(taskBorder, taskBorder, DragDropEffects.All);
-    }
-
-    private void TaskStack_Drop(object sender, DragEventArgs e)
-    {
-        var draggedTask = (FrameworkElement)e.Data.GetData(typeof(Border));
-        var fromPanel = (Panel)draggedTask.Parent;
-        var intoPanel = (Panel)sender;
-
-        fromPanel.Children.Remove(draggedTask);
-        intoPanel.Children.Add(draggedTask);
-    }
-
-    
 }
