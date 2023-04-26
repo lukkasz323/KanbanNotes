b33eecdc9eaf8cba4a31c9dec39625fdada061dd
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-04-26 17:28:25 +0200

    Add ObservableObject / Update GlobalUsings

--- KanbanNotes/GlobalUsings.cs
+++ KanbanNotes/GlobalUsings.cs
@@ -1,5 +1,7 @@
 ﻿global using System;
 global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
 global using System.Linq;
 global using System.Text;
 global using System.Windows;
---
+++ KanbanNotes/ViewModels/ObservableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace KanbanNotes.ViewModels;
+
+public class ObservableObject : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = nameof(CallerMemberNameAttribute)) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
